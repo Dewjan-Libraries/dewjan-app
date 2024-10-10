5838ff7cbe13549e81614aa79ea5ed50e7fa66a9
Deborah Amuzu <debamuzudeb@gmail.com>
2024-10-10 18:48:10 +0000

    updatee

--- src/pages/bookList/index.jsx
+++ src/pages/bookList/index.jsx
@@ -9,116 +9,106 @@ import { Link } from "react-router-dom";
 
 const Booklist = () => {
   const [books, setBooks] = useState([]);
-  const [selectedLetter, setSelectedLetter] = useState("ALL");
 
   useEffect(() => {
-    const fetchBooks = async () => {
-      const response = await axios.get(
-        "https://dewjan-api-nsqg.onrender.com/books"
-      );
-      setBooks(response.data);
-    };
+    const getBooks = async () => {
+      const response = await axios.get ("https://dewjan-api-1.onrender.com/")
+      setBooks (response.data)
 
-    fetchBooks();
-  }, 
-  
-  []);
-
-  const filterBooksByLetter = (letter) => {
-    setSelectedLetter(letter);
+    }
+    getBooks();
     
-  };
+   }, []);
 
-  const filteredBooks =
-    selectedLetter === "ALL"
-      ? books
-      : books.filter(
-          (book) => book.title.charAt(0).toUpperCase() === selectedLetter
-        );
+   return (
 
-  const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-  return (
-    <div className="bg-[url('C:\Users\debam\Desktop\react_project_theo\dewjan-app\src\assets\images\image8.png')]">
-      <div className="flex justify-around pt-10 items-center ">
-        <form className=" shadow-inner items-center flex justify-between border bg-red-600 pr-20 hover:bg-gradient-to-r from-[#cc2828] via-[#dee0ee] to-pink-600 rounded-lg text-2xl" action="">
-          <input  className=" items-center pl-10 focus:outline-none shadow-inner h-14 w-[60vw]" type="text" placeholder="search here ....."/>
-          <button className="pl-20 text-white " type="submit"><FontAwesomeIcon icon={faMagnifyingGlass} />
-          </button>
-        </form>
-        <div className="bg-gradient-to-r from-[#c78b8b] via-[#c2c3cf] to-pink-600 shadow-inner items-center  flex border h-14 w-[18vw]  px-7 rounded-lg ">
-          <Link to="/books/:id">
-            <button className="">View Single Book</button>
-          </Link>
-          </div>
-      </div>
-      <div className="flex">
-        <div className="flex pt-40 flex-col  pl-5 items-start gap-1">
-          <Link to="/">
-            <button
-              type="submit"
-              className="text-3xl text-white bg-[#070c47] border-solid py-1 px-2.5 hover:bg-gradient-to-r from-[#ac2626] via-[#f11cbc] to-red-400"
-            >
-              <FontAwesomeIcon icon={faHome} />
-            </button>
-          </Link>
-          <Link to="/">
-          <button
-            className="text-3xl text-white  bg-[#070c47] border-solid py-1 px-3 hover:bg-gradient-to-r from-[#ac2626] via-[#f11cbc] to-red-400"
-            type="submit"
-          >
-            <FontAwesomeIcon icon={faPenToSquare} />
-          </button>
-          </Link>
-          <Link to="/add-new">
-          <button
-            className="text-3xl text-white  bg-[#070c47] border-solid py-1 px-3 hover:bg-gradient-to-r from-[#ac2626] via-[#f11cbc] to-red-400"
-            types="submit"
-          >
-            <FontAwesomeIcon icon={faPlus} />
-          </button>
-          </Link>
+    <div>
+      <div>
+        {books.map((book) => (
+          <div key={book._id}>
           
-        </div>
-      
-        <div className="mx-auto h-[100vh]">
-          <div className="flex justify-center space-x-4 mb-4 pt-16">
-            {alphabet.split("").map((letter) => (
-              <button
-                key={letter}
-                className="bg-[#db2932] text-white rounded px-2 py-1 w-[80%]"
-                onClick={() => filterBooksByLetter(letter)}
-              >
-                {letter}
-              </button>
-            ))}
-            <button
-              className="bg-[#cf5b61] text-white rounded px-2 py-1"
-              onClick={() => filterBooksByLetter("ALL")}
-            >
-              ALL
-            </button>
+            <h1 className="text-2xl font-bold">{book.title}</h1>
+//                 <p className="text-[#0e0c0c] font-bold">Author: {book.author}</p>
+//                 <p className="text-[#0b0c0c} font-bold ">PublishingDate: {book.publishDate}</p>
+//                 <p className="text-[#0b0c0c] font-bold">Decriptions: {book.summary}</p>
+//                 <p className="text-[#0b0c0c] font-bold">Genre: {book.genre}</p>
+//                 <p className="text-[#0b0c0c] font-bold">Pdf: {book.pdf}</p>
           </div>
-          <div className="grid  grid-cols-1 md:grid-cols-3 gap-4">
-            {filteredBooks.map((book) => (
-              <div
-               key={book.id} className="border p-4 rounded-lg">
-                <img
-                  src={book.cover}
-                  alt={book.title}
-                  className="w-full h-48 object-cover"
-                />
-                <h1 className="text-2xl font-bold">{book.title}</h1>
-                <p className="text-[#0e0c0c] font-bold">Author: {book.author}</p>
-                <p className="text-[#0b0c0c} font-bold ">Content: {book.content}</p>
-                <p className="text-[#0b0c0c] font-bold">Decription: {book.summary}</p>
-              </div>
-            ))}
-          </div>
-        </div>
+        ) )}
       </div>
     </div>
-  );
-};
+   )
+}
+//   return (
+//     <div className="bg-[url('C:\Users\debam\Desktop\react_project_theo\dewjan-app\src\assets\images\image8.png')]">
+//       <div className="flex justify-around pt-10 items-center ">
+//         <form className=" shadow-inner items-center flex justify-between border bg-red-600 pr-20 hover:bg-gradient-to-r from-[#cc2828] via-[#dee0ee] to-pink-600 rounded-lg text-2xl" action="">
+//           <input  className=" items-center pl-10 focus:outline-none shadow-inner h-14 w-[60vw]" type="text" placeholder="search here ....."/>
+//           <button className="pl-20 text-white " type="submit"><FontAwesomeIcon icon={faMagnifyingGlass} />
+//           </button>
+//         </form>
+//         <div className="bg-gradient-to-r from-[#c78b8b] via-[#c2c3cf] to-pink-600 shadow-inner items-center  flex border h-14 w-[18vw]  px-7 rounded-lg ">
+//           <Link to="/books/:id">
+//             <button className="">View Single Book</button>
+//           </Link>
+//           </div>
+//       </div>
+//       <div>
+//   key={index}
+//   <img 
+//   src={book.image} 
+//   alt="image" />
+//   <h1 className="text-2xl font-bold">{book.title}</h1>
+//                 <p className="text-[#0e0c0c] font-bold">Author: {book.author}</p>
+//                 <p className="text-[#0b0c0c} font-bold ">PublishingDate: {book.publishDate}</p>
+//                 <p className="text-[#0b0c0c] font-bold">Decriptions: {book.summary}</p>
+//                 <p className="text-[#0b0c0c] font-bold">Genre: {book.genre}</p>
+//                 <p className="text-[#0b0c0c] font-bold">Pdf: {book.pdf}</p>
+// </div>
+//       <div className="flex">
+//         <div className="flex pt-40 flex-col  pl-5 items-start gap-1">
+//           <Link to="/">
+//             <button
+//               type="submit"
+//               className="text-3xl text-white bg-[#070c47] border-solid py-1 px-2.5 hover:bg-gradient-to-r from-[#ac2626] via-[#f11cbc] to-red-400"
+//             >
+//               <FontAwesomeIcon icon={faHome} />
+//             </button>
+//           </Link>
+//           <Link to="/">
+//           <button
+//             className="text-3xl text-white  bg-[#070c47] border-solid py-1 px-3 hover:bg-gradient-to-r from-[#ac2626] via-[#f11cbc] to-red-400"
+//             type="submit"
+//           >
+//             <FontAwesomeIcon icon={faPenToSquare} />
+//           </button>
+//           </Link>
+//           <Link to="/add-new">
+//           <button
+//             className="text-3xl text-white  bg-[#070c47] border-solid py-1 px-3 hover:bg-gradient-to-r from-[#ac2626] via-[#f11cbc] to-red-400"
+//             types="submit"
+//           >
+//             <FontAwesomeIcon icon={faPlus} />
+//           </button>
+//           </Link>
+          
+//         </div>
+      
+//         <div className="mx-auto h-[100vh]">
+//           <div className="flex justify-center space-x-4 mb-4 pt-16">
+//             {books.map((book, index) => {
+
+
+          
+
+//           )
+//             })
+//           }
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 
 export default Booklist;
